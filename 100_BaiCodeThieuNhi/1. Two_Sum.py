c034a3fa1005eb3bfde0824f6ba01955f655a49b
Dinhnhungs <dinhnhungs272@gmail.com>
2023-06-27 15:07:24 +0700

    Update 1. Two_Sum.py

--- 100_BaiCodeThieuNhi/1. Two_Sum.py
+++ 100_BaiCodeThieuNhi/1. Two_Sum.py
@@ -2,14 +2,14 @@
 
 # You may assume that each input would have exactly one solution.
 
-# Input: numbers={2, 7, 11, 15}, target=9 Output: index1=1, index2=2
-print("Please type an array of integers: ")
-nums = [int(x) for x in input().split()]
-target = int(input("Type a target number: "))
+# Input: numbers=[2, 7, 11, 15], target=9 Output: [0,1]
 
-for i in range (0, len(nums)):
-    for j in range (1, len(nums)):
-        if int(nums[i]) + int(nums[j]) == target:
-            print([i,j])
-            quit()
+print('Please type an array: ')
 
+arr = [int(a) for a in input().split()] 
+target = int(input('type a target number: '))
+print(arr)
+for i in range(0,len(arr)) :
+    for j in range(1,len(arr)) :
+        if int(arr[i]) + int(arr[j]) == target :
+            print(i,j)
