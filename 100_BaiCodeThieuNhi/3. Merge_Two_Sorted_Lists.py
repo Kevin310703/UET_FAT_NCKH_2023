e793bfed103246a48020a2ab0af65422f0f133fc
Dinhnhungs <dinhnhungs272@gmail.com>
2023-06-27 15:34:59 +0700

    Update 3. Merge_Two_Sorted_Lists.py

--- 100_BaiCodeThieuNhi/3. Merge_Two_Sorted_Lists.py
+++ 100_BaiCodeThieuNhi/3. Merge_Two_Sorted_Lists.py
@@ -9,3 +9,10 @@ Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]
 
 """
+
+lst1 = [int(a) for a in input().split()]
+print(lst1)
+lst2 = [int(b) for b in input().split()]
+print(lst2)
+lst = lst1 + lst2 
+print(lst)
