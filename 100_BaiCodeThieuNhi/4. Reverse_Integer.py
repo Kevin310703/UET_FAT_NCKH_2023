cae6dbe9b2c8ec1c141a99d4c48967789d932832
Dinhnhungs <dinhnhungs272@gmail.com>
2023-07-02 23:30:18 +0700

    Update 4. Reverse_Integer.py

--- 100_BaiCodeThieuNhi/4. Reverse_Integer.py
+++ 100_BaiCodeThieuNhi/4. Reverse_Integer.py
@@ -1,45 +1,20 @@
-"""
-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value 
-to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
-
-Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
-
-Example 1:
-
-    Input: x = 123
-    Output: 321
-
-Example 2:
-
-    Input: x = -123
-    Output: -321
-
-Example 3:
-
-    Input: x = 120
-    Output: 21
-
-"""
-def solatnguoc(n) :
-    if n < 0 :
-        soam = True 
-        n = abs(n)
-    else :
-        soam = False 
-
-
-    lat = 0
-    while n != 0 :
-         lat = lat*10 + n%10
-         n //= 10
+def reverse(x):
+    if x < 0:
+        is_negative = True
+        x = abs(x)
+    else:
+        is_negative = False
     
-    if soam : 
-        lat *= -1
-    if soam < -2**31 or soam > (2**31-1) :
+    reversed_num = 0
+    while x != 0:
+        last_digit = x % 10
+        reversed_num = (reversed_num * 10) + last_digit
+        x = x // 10
+    
+    if is_negative:
+        reversed_num *= -1
+    
+    if reversed_num < -2**31 or reversed_num > (2**31 - 1):
         return 0
-    return lat
-
-
-
     
-    
+    return reversed_num
